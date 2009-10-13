78dd13e9e25dfd9ef6de28c939eb09a6bfbb0f95
altaokami <altaokami@gmail.com>
2009-10-13 02:27:14 +0000

    Fixed status attribute and xml output for login/out.

--- api.post.logout.php
+++ api.post.logout.php
@@ -13,24 +13,31 @@
 
 	class postLogout extends CallWrapper
 	{
-		protected function initJSON()
+		private function logout()
 		{
 			if( !isset($_SESSION['logged']) ) throw new Exception("User is not logged in");
 
 			require_once( 'lib.userlog.php' );
 
-			//
+			return UserLog::out();
+		}
+
+		protected function initJSON()
+		{
+			$logged = $this->logout();
+
 			// include the json
 			$o = new JSON( $this->time );
 
-			if( $ver = UserLog::out() )
+			if( $logged )
 			{
-				$o->valid = 1;
+				$o->status = "ok";
 				$o->message = "You are now logged out.";
 			}
 			else
 			{
-				$o->valid = 0;
+				$o->status = "error";
+				$o->message = "User was not logged out";
 			}
 
 			return $o;
@@ -39,7 +46,31 @@
 
 		protected function initXML()
 		{
-			return $this->initJSON();
+			$logged = $this->logout();
+
+			require_once('class.xmlcontainer.php');
+
+			$resp = new XMLContainer("1.0","utf-8",$this->time);
+
+			$root = $resp->createElement("response");
+
+			if( $logged )
+			{
+				$status = "ok";
+				$message = "You are now logged out.";
+			}
+			else
+			{
+				$status = "error";
+				$message = "User was not logged out";
+			}
+
+			$root->setAttribute("status", $status);
+			$root->appendChild( $resp->createElement("message", $message) );
+
+			$resp->appendChild($root);
+
+			return $resp;
 		}
 	}
 
