02052948f8c596975a1e37f11a47eefdfa1c9b3f
altaokami <altaokami@gmail.com>
2009-11-06 03:56:41 +0000

    Added reconstruction joins for packet queries.

--- class.nupacketquery.php
+++ class.nupacketquery.php
@@ -15,7 +15,14 @@
     function __construct($publisher, $page=1,$limit=20, $order='desc')
     {
       parent::__construct('nu_packet_index as P force index(publisher)');
-      $this->field( array('P.id as packet','P.ts') );
+      $this->field( array(
+	  'P.id as packet','P.ts','P.publisher','N.name','D.name as domain'
+      ));
+
+      $this->join( "nu_user as U",	    "U.id=P.publisher" );
+      $this->join( "nu_name as N",	    "N.id=U.name" );
+      $this->join( "nu_domain as D",	    "D.id=U.domain" );
+
       $this->where("P.publisher={$publisher}");
       $this->order( 'P.ts', isType('asc|desc', $order)? $order : 'desc' );
       $this->page( $page, $limit, 20, 10, 100 );
@@ -37,7 +44,15 @@
     function __construct($subscriber, $page=1, $limit=20, $order='desc')
     {
       parent::__construct('nu_packet_inbox as P');
-      $this->field( array('P.packet','P.ts') );
+      $this->field( array(
+	  'P.packet','P.ts','I.publisher','N.name','D.name as domain'
+      ));
+
+      $this->join( "nu_packet_index as I",  "I.id=P.packet" );
+      $this->join( "nu_user as U",	    "U.id=I.publisher" );
+      $this->join( "nu_name as N",	    "N.id=U.name" );
+      $this->join( "nu_domain as D",	    "D.id=U.domain" );
+
       $this->where( "P.subscriber={$subscriber}");
       $this->order( 'P.ts', isType('asc|desc', $order)? $order : 'desc' );
       $this->page(  $page, $limit, 20, 10, 100 );
