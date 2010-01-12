4ec344b214e536dba37e2c14a85bb44942daa45d
altaokami@gmail.com <altaokami@gmail.com@57cc54f0-98d7-11de-9cb2-11d8f9f316f8>
2010-01-12 03:52:59 +0000

    Tidy account/tokens request method. Added pack method to KeyObject.

--- trunk/api.get.accounttokens.php
+++ trunk/api.get.accounttokens.php
@@ -4,32 +4,26 @@
     get listing of keys
   */
 
-  require_once( 'abstract.callwrapper.php' );
+  require_once( 'api.class.userauthmethod.php' );
+  require_once( 'lib.keys.php' );
 
-  class getTokens extends CallWrapper
+  class getTokens extends apiUserAuthMethod
   {
     private function query($user)
     {
-      $q = "select id, auth_key, ts from nuclear_api_auth where user={$user};";
-      return WrapMySQL::q($q, "Unable to query auth keys");
-    }
-
-    private function getUser()
-    {
-      $user_id = $GLOBALS['USER_CONTROL']['id'];
-      if( !$user_id )
-	throw new Exception("Unauthorized", 2);
-      return $user_id;
+      return WrapMySQL::q(
+                "select id, LOWER(HEX(auth)) as auth, ts ".
+                "from nuclear_api_auth ".
+                "where user={$user};", 
+                "Unable to query auth keys");
     }
 
     protected function initJSON()
     {
-      $user_id = $this->getUser();
-
-      $result = $this->query($user_id);
+      $user = $this->getAuth();
+      $result = $this->query($user->id);
 
       $resp = new JSON( $this->time );
-
       $resp->status = "ok";
 
       $keys = array();
@@ -49,31 +43,29 @@
 
     protected function initXML()
     {
-      $user_id = $this->getUser();
+      $user = $this->getAuth();
+      $result = $this->query($user->id);
 
-      $result = $this->query($user_id);
+      require_once('class.xmlresponse.php');
+      $resp = new XMLResponse($this->time );
 
-      require_once('class.xmlcontainer.php');
-      $resp = new XMLContainer("1.0","utf-8", $this->time );
-      $root = $resp->createElement("response");
-      $root->setAttribute("status","ok");
-      $root->setAttribute("request","get.tokens");
+      $resp->status = "ok";
+      $resp->request= "get.account/tokens";
 
-      $atts = array("id","auth_key","timestamp");
       while($row = mysql_fetch_row($result))
       {
-	$key = $resp->createElement("token");
-	foreach($atts as $i=>$a)
-	  $key->setAttribute($a,$row[$i]);
-	
-	$root->appendChild($key);
+        $token = $resp->attach("token");
+        $token->setAttribute( "id", $row[0] );
+        $token->setAttribute( "timestamp", $row[2] );
+        $token->setAttribute( "auth", KeyObject::pack($row[1]) );
+
+        $resp->append( $token );
       }
 
-      $resp->appendChild($root);
       return $resp;
     }
   }
 
-  return getTokens;
+  return "getTokens";
 
 ?>
