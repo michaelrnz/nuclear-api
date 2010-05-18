fedbdfa6b7df56fa591d63440920a7af397e3a19
altaokami <altaokami@gmail.com>
2010-05-18 21:53:30 +0000

    Default template Webfinger XRD method.

---
+++ api.get.xrd.webfinger.php
@@ -0,0 +1,52 @@
+<?php
+
+    require_once('abstract.apimethod.php');
+    require_once('class.webfinger.php');
+    require_once('class.events.php');
+    require_once('class.xmlcontainer.php');
+
+    class getWebFingerXRD extends NuclearAPIMethod
+    {
+        protected function build()
+        {
+            $uri    = $this->call->uri;
+            $webf   = Webfinger::getInstance();
+            $acct   = $webf->parse( $uri );
+
+            if( !$acct )
+                throw new WebfingerException('Invalid uri');
+            $events = Events::getInstance();
+
+            $resp   = new XMLContainer('1.0', 'UTF-8');
+
+            //
+            // append the XRD root
+            //
+            $xrd    = $resp->createElement('XRD');
+            $xrd->createAttribute('xmlns', 'http://docs.oasis-open.org/ns/xri/xrd-1.0');
+            $resp->appendChild($xrd);
+
+            $xrd->appendChild(
+                $resp->createElement(
+                    'Subject',
+                    "acct:{$acct['user']}@{$acct['domain']}"
+                )
+            );
+
+            $xrd->appendChild(
+                $resp->createElement(
+                    'Alias',
+                    "http://{$acct['domain']}/{$acct['user']}"
+                )
+            );
+
+            // filter through observers with acct
+            $resp   = $events->filter( 'nu_webfinger_xrd', $resp, $acct );
+
+            return $resp;
+        }
+    }
+
+    return "getWebFingerXRD";
+
+?>
