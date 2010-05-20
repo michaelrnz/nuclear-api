576c695c58868a00e02aa6ea86c3f068d5b1e4a8
altaokami@gmail.com <altaokami@gmail.com@57cc54f0-98d7-11de-9cb2-11d8f9f316f8>
2010-05-20 00:54:41 +0000

    Added Domain class for db modeling. Slightly tested.

--- trunk/class.domains.php
+++ trunk/class.domains.php
@@ -15,15 +15,103 @@
     require_once('interface.nuclear.php');
     require_once('class.io.php');
 
+    class Domain extends Object
+    {
+        public $id;
+        public $name;
+        protected $webfinger;
+        protected $portal;
+
+        function __get( $f )
+        {
+            if( isType( 'webfinger|portal', $k )  && !is_null( $this->$k ) )
+            {
+                return "http://" . $this->$k;
+            }
+
+            return null;
+        }
+
+        function __set( $k, $v )
+        {
+            if( isType( 'webfinger|portal', $k ) )
+            {
+                $this->$k   = str_replace('http://', '', $v);
+            }
+        }
+
+        public function load()
+        {
+            $name   = safe_slash( $this->name );
+            $data   = Database::getInstance()->single(
+                        "select * from ". nu_db() . "nu_domain ".
+                        "where name='{$name}' limit 1;",
+                        "Error loading Domain" );
+
+            if( $data )
+                $this->merge( $data );
+
+            return $this;
+        }
+
+        public function save()
+        {
+            $fields     = array();
+            $values     = array();
+            $dups       = array();
+
+            if( is_null($this->name) )
+                return $this;
+
+            $fields[]   = 'name';
+            $values[]   = "'". safe_slash( $this->name ) ."'";
+
+            if( !is_null($this->id) )
+            {
+                $fields[]   = 'id';
+                $values[]   = $this->id;
+            }
+
+            if( !is_null($this->webfinger) )
+            {
+                $fields[]   = 'webfinger';
+                $values[]   = "'". safe_slash( $this->webfinger ) ."'";
+                $dups[]     = "webfinger=values(webfinger)";
+            }
+
+            if( !is_null($this->portal) )
+            {
+                $fields[]   = 'portal';
+                $values[]   = "'". safe_slash( $this->portal ) ."'";
+                $dups[]     = "portal=values(portal)";
+            }
+
+            $sql =  "insert into ". nu_db() ."nu_domain ".
+                    "(". implode(', ', $fields) . ") ".
+                    "values (". implode(', ', $values) .") ";
+
+            if( count($dups)>0 )
+            {
+                $sql .=  "on duplicate key update ". implode(', ', $dups);
+            }
+
+            Database::getInstance()->execute( $sql, "Error saving Domain" );
+
+            return $this;
+        }
+    }
+
     class Domains implements iSingleton
     {
         private static $_instance;
         private $db;
         private $io;
+        private $xrd;
+        private $domain;
 
         function __construct()
         {
-            //$this->db   = Database::getInstance();
+            $this->db   = Database::getInstance();
             $this->io   = IO::getInstance();
         }
 
@@ -44,8 +132,20 @@
             return trim(str_replace("'","", $domain),"/ \r\n");
         }
 
-        public function host_meta( $domain )
+        public function host_meta( $domain=null )
         {
+            if( is_null($domain) )
+            {
+                if( is_null($this->domain) )
+                {
+                    return false;
+                }
+                else
+                {
+                    $domain = $this->domain->name;
+                }
+            }
+
             $resource   = "http://" . $this->clean($domain) . "/.well-known/host-meta";
             $xrd_data   = $this->io->uri( $resource );
 
@@ -53,14 +153,43 @@
             {
                 require_once('class.domdocumentexceptor.php');
 
-                $doc = new DOMDocumentExceptor('1.0', 'utf-8');
-                $doc->loadXML( $xrd_data );
-                return $doc;
+                $this->xrd = new DOMDocumentExceptor('1.0', 'utf-8');
+                $this->xrd->loadXML( $xrd_data );
+
+                return $this->xrd;
             }
 
             return false;
         }
 
+        public function template( $rel )
+        {
+            if( $this->xrd )
+            {
+                $links = $this->xrd->getElementsByTagName('Link');
+                foreach( $links as $link )
+                {
+                    if( $link->getAttribute('rel') == $rel )
+                    {
+                        return $link->getAttribute('template');
+                    }
+                }
+            }
+
+            return false;
+        }
+
+        public function load( $domain, $create=false )
+        {
+            $domain_obj = new Domain( array('name'=> $this->clean($domain) ) );
+            $domain_obj->load();
+
+            if( !$domain_obj->id && $create )
+                $domain_obj->save();
+
+            $this->domain = $domain_obj;
+            return $domain_obj;
+        }
     }
 
 
