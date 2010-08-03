abe421d4dac915fe37796a8730695ef42631bc6a
altaokami@gmail.com <altaokami@gmail.com@57cc54f0-98d7-11de-9cb2-11d8f9f316f8>
2010-08-03 22:01:50 +0000

    Internal structure and authorization changes to Nuclear.

--- trunk/class.nuclearapi.php
+++ trunk/class.nuclearapi.php
@@ -14,9 +14,14 @@
     {
         protected static $_instance;
         public static $_gv = "APICALL";
-
+        protected $resolution;
+        
         /* iSingleton interfacing */
-
+        public function __construct()
+        {
+            $this->resolution = array();
+        }
+        
         public static function getInstance()
         {
             if( is_null( self::$_instance ) )
@@ -24,18 +29,18 @@
 
             return self::$_instance;
         }
-
+        
         public static function setInstance( &$object )
         {
             if( is_a( $object, "NuclearAPI" ) )
                 self::$_instance = $object;
-
+            
             return self::$_instance;
         }
-
-
+        
+        
         /* anonymous caller  // api method generation */
-
+        
         //
         // We call the api method file dynamically
         //
@@ -46,8 +51,8 @@
                 $rest               = strtolower($method_match[1]);
                 $method         = $method_match[2];
                 $method[0]  = strtolower($method[0]);
-
-                return self::execute( $rest, $method, $args[0], $args[1] );
+                
+                return $this->execute( $rest, $method, $args[0], $args[1] );
             }
             else
             {
@@ -69,7 +74,7 @@
         // call the api, via include
         // TODO handle for dynamic output types
         //
-        private static function &execute( $rest, $method, &$call, $output="json" )
+        private function &execute( $rest, $method, &$call, $output="json" )
         {
             // name the src
             $src_1  = "api.{$rest}.". strtolower($method) .".php";
@@ -80,30 +85,50 @@
             self::globalize( $call );
 
             //
+            // test resolution
+            if( array_key_exists( md5($src_1), $this->resolution ) )
+            {
+                $api_class  = $this->resolution[ md5($src_1) ];
+                $dynamic    = false;
+            }
+            else if( array_key_exists( md5($src_2), $this->resolution ) )
+            {
+                $api_class  = $this->resolution[ md5($src_2) ];
+                $dynamic    = true;
+            }
+            else
+            {
+
+            //
             // try include
             $api_class  = (@include $src_2);
-
+            
             if( !$api_class )
             {
                 $api_class  = (@include $src_1);
                 $dynamic    = false;
+
+                $this->resolution[ md5($src_1) ] = $api_class;
             }
             else
             {
                 $dynamic    = true;
+                $this->resolution[ md5($src_2) ] = $api_class;
             }
 
+            }
+            
             if( $api_class && strlen($api_class)>1 )
             {
                     if( class_exists( $api_class, false ) )
                     {
                         try
                         {
-                            if( $dynamic || is_subclass_of( $api_class, 'NuclearAPIMethod') )
+                            if( $dynamic || is_subclass_of($api_class, 'NuclearAPIMethod') )
                                 $co = new $api_class( microtime(true), false );
                             else
                                 $co = new $api_class( microtime(true), $output, false );
-
+                            
                             return $co->response;
                         }
                         catch( Exception $e )
@@ -127,12 +152,12 @@
                 $o->status      = "error";
                 $o->message = "Operation does not exist: {$method}";
             }
-
+            
         }
 
 
         //
-        // get/set global field index
+        // get/set global field index 
         // index of call
         //
         public static function setGlobal($s)
