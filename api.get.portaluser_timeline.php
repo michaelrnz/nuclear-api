7377f12ff94cda44b2313076662bc4812ecd8540
altaokami <altaokami@gmail.com>
2010-08-30 20:56:49 +0000

    Portal modifications, adjusted for post/update/delete activity verbs on local. Accepts JSON from local, decodes magic envelope data. ME is unauthorized, needs API auth module.

--- api.get.portaluser_timeline.php
+++ api.get.portaluser_timeline.php
@@ -12,47 +12,47 @@
 
     require_once( 'api.class.usermethod.php' );
     require_once( 'lib.entryquery.php' );
-    
+
     class postPortalUserTimeline extends NuUserMethod
     {
         protected function build()
         {
             $author = $this->getUser();
-            $query  = new EntryUserTimelineQuery( 
-                        $author->id, 
-                        $this->call->page, 
+            $query  = new EntryUserTimelineQuery(
+                        $author->id,
+                        $this->call->page,
                         $this->call->limit );
-            
+
             $timeline   = array();
             $events     = Events::getInstance();
             $do_filter  = $events->isObserved('portal_timeline_entry');
-            
+
             if( $query->select() )
             {
                 require_once('class.entry.php');
-                
+
                 while( $data = $query->object() )
                 {
-                    $entry_obj      = unserialize( $data->entry_data );
-                    
+                    $entry_obj      = json_decode( $data->entry_data );
+
                     $entry          = new Entry( $entry_obj );
                     $entry->id      = $data->entry_id;
                     $entry->guid    = to_base( from_hex( $data->entry_guid ) );
                     $entry->published   = date('Y-m-d\TH:i:s\Z', $data->entry_published);
                     $entry->updated     = date('Y-m-d\TH:i:s\Z', $data->entry_updated);
-                    
+
                     $entry->author  = new Author( array(
-                                        'id'=>$data->author_id, 
-                                        'name'=>$data->author_name, 
+                                        'id'=>$data->author_id,
+                                        'name'=>$data->author_name,
                                         'domain'=>$data->author_domain) );
-                    
+
                     if( $do_filter )
                         $entry      = $events->filter( 'portal_timeline_entry', $entry, $data );
-                    
+
                     array_push( $timeline, $entry );
                 }
             }
-            
+
             return $timeline;
         }
     }
--- lib.portal.php
+++ lib.portal.php
@@ -35,6 +35,9 @@
             return self::$_instance;
         }
 
+        //
+        // Take int timestamps for creating
+        //
         public function create( $publisher, $published, $updated=false, $guid=false )
         {
             if( !$updated )
@@ -48,14 +51,19 @@
                         "values ({$publisher}, UNHEX(REVERSE('{$guid}')), {$published}, {$updated}) ";
 
             $this->id       = $this->db->id( $sql, "Error creating portal entry (index)" );
-            $this->guid     = $guid;
-            $this->published= $published;
-            $this->updated  = $updated;
+            $this->guid     = to_base( from_hex($guid) );
+
+            // do not use gmdate because the timestamps are already utc
+            $this->published= date('Y-m-d\TH:i:s\Z', $published);
+            $this->updated  = date('Y-m-d\TH:i:s\Z', $updated);
 
             return $this->id;
         }
 
-        public function update( $entry_id, $publisher, $published=false, $updated=false, $guid=false )
+        //
+        // TODO why do we need GUID in update?
+        //
+        public function updateIndex( $entry_id, $publisher, $published=false, $updated=false, $guid=false )
         {
             $fields = array('id','publisher');
             $values = array($entry_id, $publisher);
@@ -85,12 +93,23 @@
             if( count($dups)==0 )
                 return 0;
 
-            $sql =      "insert into ". nu_db() ."nu_entry_index ".
+            $sql =      "insert into ". nu_db() ."nu_entry_index (".
                         implode(', ', $fields).
-                        "values (". implode(', ', $values) .") ".
+                        ") values (". implode(', ', $values) .") ".
                         "on duplicate key update ". implode(', ', $dups);
 
             return $this->db->affected( $sql, "Error updating portal entry (index)" );
+        }
+
+        public function delete( $entry_id )
+        {
+            $this->db->void(
+                "delete from nu_entry_data where entry={$entry_id} limit 1",
+                "Error removing entry_data");
+
+            $this->db->void(
+                "delete from nu_entry_index where id={$entry_id} limit 1",
+                "Error removing entry_index");
         }
 
         public function guid()
@@ -99,25 +118,22 @@
             return $guid[0];
         }
 
-        public function downmix( $entry, $entry_id=null )
+        public function setObject( $entry_id, $entry )
         {
-            $entry_id = !is_null($entry_id) ? $entry_id : $this->id;
-
-            // downmix current dynamic attributes
-            $entry->id      = $entry_id;
-            $entry->guid    = $this->guid;
-            $entry->published= $this->published;
-            $entry->updated = $this->updated;
-
-            // emit unidentified entry to observers
-            if( $this->events->isObserved('portal_publish_identified') )
-                $entry = $this->events->filter('portal_publish_identified', $entry, $entry_id);
+            $restore = array();
 
             // remove dynamic attributes from entry
-            unset($entry->id);
-            unset($entry->guid);
-            unset($entry->published);
-            unset($entry->updated);
+            foreach( array('id','guid','published','updated') as $f )
+            {
+                $restore[$f] = $entry->$f;
+                unset($entry->$f);
+            }
+
+            if( $entry->ns_activity_verb == 'http://activitystrea.ms/schema/1.0/post' || $entry->ns_activity_verb == 'http://activitystrea.ms/schema/1.0/update' )
+            {
+                $verb = $entry->ns_activity_verb;
+                unset($entry->ns_activity_verb);
+            }
 
             // downmix entry to data
             $data   = json_encode( $entry );
@@ -133,12 +149,30 @@
             }
 
             // restore dynamic attributes for echo
-            $entry->id          = $this->id;
-            $entry->guid        = to_base( from_hex($this->guid) );
-            $entry->published   = date('Y-m-d\TH:i:s\Z', $this->published);
-            $entry->updated     = date('Y-m-d\TH:i:s\Z', $this->updated);
+            foreach( array('id','guid','published','updated') as $f )
+            {
+                $entry->$f  = $restore[$f];
+            }
+
+            if( !isset($entry->ns_activity_verb) )
+                $entry->ns_activity_verb = $verb;
 
             return $entry;
+        }
+
+        public function downmix( $entry_id, $entry )
+        {
+            // downmix current dynamic attributes
+            $entry->id          = $entry_id;
+            $entry->guid        = $this->guid;
+            $entry->published   = $this->published;
+            $entry->updated     = $this->updated;
+
+            // emit unidentified entry to observers
+            if( $this->events->isObserved('portal_publish_identified') )
+                $entry = $this->events->filter('portal_publish_identified', $entry, $entry_id);
+
+            return $this->setObject( $entry_id, $entry );
         }
     }
 
@@ -167,50 +201,117 @@
             return self::$_instance;
         }
 
-        public function publish( $publisher, $entry, $isLocal )
+        public function post( $publisher, $entry, $isLocal )
         {
-            $this->mode         = "publish";
+            $this->mode         = "post";
             $this->isLocal      = $isLocal;
             $this->publisher    = $publisher;
             $this->entry        = $entry;
-            $this->entry_id     = $this->identify();
+            $this->entry_id     = $this->create();
 
-            $entry              = PortalEntry::getInstance()->downmix( $this->entry );
+            $entry              = PortalEntry::getInstance()->downmix( $this->entry_id, $this->entry );
 
             // emit published, use for local subs
-            $this->events->emit('portal_published', $entry, $publisher);
+            $this->events->emit('portal_posted', $entry, $publisher);
 
             // emit published local, use for dispatch
-            if( $this->isLocal )
-                $this->events->emit('portal_published_local', $entry, $publisher);
+            if( $isLocal )
+                $this->events->emit('portal_post_local', $entry, $publisher);
 
             return $entry;
         }
 
         public function update( $publisher, $entry, $isLocal )
         {
+            $this->mode         = "update";
+            $this->isLocal      = $isLocal;
+            $this->publisher    = $publisher;
+            $this->entry        = $entry;
+            $this->entry_id     = $this->identify();
+
+            // update the meta
+            PortalEntry::getInstance()->updateIndex(
+                $this->entry_id,
+                $publisher->id,
+                $this->timestamp($entry->published),
+                $this->timestamp($entry->updated) );
+
+            // set the object
+            PortalEntry::getInstance()->setObject( $this->entry_id, $entry );
+
+            // emit published, use for local subs
+            $this->events->emit('portal_updated', $entry, $publisher);
+
+            // emit published local, use for dispatch
+            if( $isLocal )
+                $this->events->emit('portal_update_local', $entry, $publisher);
+
+            return $entry;
         }
 
-        public function unpublish( $publisher, $entry_guid, $isLocal )
+        public function delete( $publisher, $entry, $isLocal )
         {
+            $this->mode         = "delete";
+            $this->isLocal      = $isLocal;
+            $this->publisher    = $publisher;
+            $this->entry        = $entry;
+            $this->entry_id     = $this->identify();
+
+            PortalEntry::getInstance()->delete( $this->entry_id );
+
+            // emit published, use for local subs
+            $this->events->emit('portal_deleted', $entry, $publisher);
+
+            // emit published local, use for dispatch
+            if( $isLocal )
+                $this->events->emit('portal_delete_local', $entry, $publisher);
+        }
+
+        public function create()
+        {
+            // adjust for providing only published/updated
+            if( $this->entry->published && !isset($this->entry->updated) )
+            {
+                $this->entry->updated = $this->entry->published;
+            }
+
+            $entry_id = PortalEntry::getInstance()->create(
+                            $this->publisher->id,
+                            $this->timestamp( $this->entry->published ),
+                            $this->timestamp( $this->entry->updated ),
+                            $this->guid( $this->entry->guid ) );
+
+            return $entry_id;
         }
 
         public function identify()
         {
-            $id = is_null($this->entry->id) ? intval($id) : null;
+            $id     = isset($this->entry->id) ? intval($this->entry->id) : null;
+            $guid   = $this->entry->guid;
 
-            if( $this->isLocal && $id>0 )
+            if( is_null($id) && $guid )
             {
-                if( $this->mode == "publish" )
-                    throw new Exception("Entry id detected on local publishing portal");
+                $guid = $this->guid( $guid );
+                $data = $this->db->single(
+                            "select id from ". nu_db() . "nu_entry_index ".
+                            "where publisher={$this->publisher->id} && guid=UNHEX(REVERSE('{$guid}') ".
+                            "limit 1");
 
-                $owner  = $this->db->single(
+                if( !is_array($data) )
+                    throw new Exception("Could not identify entry {$this->entry->guid}");
+
+                return $data[0];
+            }
+
+            if( $id>0 )
+            {
+                $data = $this->db->single(
                             "select id from ". nu_db() ."nu_entry_index ".
                             "where id={$id} && publisher={$this->publisher->id} ".
                             "limit 1");
 
-                if( !is_array($owner) )
-                    throw new Exception("Unauthorized portal publisher for entry {$id}", 2);
+                if( !is_array($data) )
+                    throw new Exception("Could not identify entry {$id}", 2);
 
                 return $id;
             }
@@ -219,39 +320,24 @@
             if( $this->events->isObserved('portal_publish_unidentified') )
                 $this->events->emit('portal_publish_unidentified', $this->entry, $this->publisher);
 
-            $has_published  = !is_null($this->entry->published);
-            $has_updated    = !is_null($this->entry->updated);
-
-            if( $has_published && !$has_updated )
-            {
-                $auto = false;
-            }
-            else if( $has_updated && !$has_published )
-            {
-                $this->entry->published = $this->entry->updated;
-            }
-            else
-            {
-                $auto = NU_ACCESS_TIME;
-            }
-
-            $entry_id = PortalEntry::getInstance()->create(
-                            $this->publisher->id,
-                            $this->timestamp( $this->entry->published, $auto ),
-                            $this->timestamp( $this->entry->updated, $auto ),
-                            $this->guid( $this->entry->guid ) );
-
-            return $entry_id;
+            return null;
         }
 
         public function timestamp( $str=null, $auto=NU_ACCESS_TIME )
         {
             if( !is_null($str) )
             {
+                if( substr($str,-1)==='Z' )
+                    $str = substr($str,0,-1) .'+0000';
+
                 $ts = strtotime( $str );
+
+                // adjust for server DST
+                if( date("I", $ts)==1 )
+                    $ts -= 3600;
             }
 
-            return $ts ? $ts : $auto;
+            return gmstrftime("%s", $ts ? $ts : $auto);
         }
 
         public function guid( $guid )
